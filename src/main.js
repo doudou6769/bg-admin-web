3e57aaae4192d81ac2f63a4d83195d2aae5f7862
doudou <13653506769@163.com>
2020-10-04 21:01:14 +0800

    '项目集成ivew'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import iVew from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
+Vue.use(iVew);
 
 Vue.config.productionTip = false
 
